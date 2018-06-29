d5e221d07b7d73ad8c83d3945e569b84a4b4b4a9
Apoorv-Gupta1396 <ayush.singh.xda@gmail.com>
2018-06-29 17:46:52 +0530

    nav reactive to routes, Landing with typed, parallax

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
+import Parallax from 'parallax-js';
+import * as Typed from 'typed.js';
 
 @Component({
   selector: 'app-home',
@@ -14,7 +16,23 @@ export class HomeComponent implements OnInit {
   benefits = ['Enhance Mood','Reduce Stress','Become Sexy','Get Stronger','Improve Heart Health','Become Confident'];
 
   ngOnInit() {
-    this.titleService.setTitle(this.route.snapshot.data['title'])
+    this.titleService.setTitle(this.route.snapshot.data['title']);
+    var scene = document.getElementById('parallaxdoodle');
+    var parallaxInstance = new Parallax(scene);
+
+    var typed = new Typed("#landingquotes", {
+      strings: [
+        "One religion - Fitness!", 
+        "Strong is the new Sexy!",
+        "Be Stronger than your excuses!",
+        "Your only limit is you!"
+      ],
+      typeSpeed: 50,
+      backSpeed: 50,
+      backDelay: 5000,
+      loop: true,
+      showCursor: false
+  });
   }
 
 }
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
+import { Router, NavigationStart } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -7,7 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  previousUrl: string = 'home';
+
+  constructor(private renderer: Renderer2, private router: Router) {
+    this.router.events
+    .subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        if (this.previousUrl) {
+          this.renderer.addClass(document.getElementById('navbar'), 'home');
+        }
+        let currentUrlSlug = event.url.slice(1)
+        console.log(currentUrlSlug)
+        if (currentUrlSlug) {
+          this.renderer.removeClass(document.getElementById('navbar'), 'home');
+        }
+        this.previousUrl = currentUrlSlug;
+      }
+    });
+   }
 
   ngOnInit() {
   }
