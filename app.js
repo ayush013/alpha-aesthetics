0b383dc2120da4ac725c5c5406958aff9ad2793e
Ayush Singh <ayush.singh.xda@gmail.com>
2020-04-20 23:03:22 +0530

    commented code to only serve static website

--- app.js
+++ app.js
@@ -1,62 +1,62 @@
 const express = require('express');
-const helmet = require('helmet');
+// const helmet = require('helmet');
 const path = require('path');
-const authRoutes = require('./routes/auth-routes');
-const dashboardRoutes = require('./routes/dashboard');
-const authGuardRoute = require('./routes/auth-guard');
-const passportgoogleSetup = require('./config/passport-setup-google');
-const passportfacebookSetup = require('./config/passport-setup-facebook');
-const mongoose = require('mongoose');
-const keys = require('./config/keys');
-const cookieSession = require('cookie-session');
-const passport = require('passport');
-const bodyParser = require('body-parser');
-var favicon = require('serve-favicon');
+// const authRoutes = require('./routes/auth-routes');
+// const dashboardRoutes = require('./routes/dashboard');
+// const authGuardRoute = require('./routes/auth-guard');
+// const passportgoogleSetup = require('./config/passport-setup-google');
+// const passportfacebookSetup = require('./config/passport-setup-facebook');
+// const mongoose = require('mongoose');
+// const keys = require('./config/keys');
+// const cookieSession = require('cookie-session');
+// const passport = require('passport');
+// const bodyParser = require('body-parser');
+// var favicon = require('serve-favicon');
 
 // EXPRESS INITIALIZATION
 const app = express();
 
-// HELMET 
-app.use(helmet());
+// // HELMET 
+// app.use(helmet());
 
-// BODYPARSER
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+// // BODYPARSER
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: false}));
 
 // FAVICON + PUBLIC DIR
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(express.static('public'));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(express.static(__dirname + '/public'));
 
-// COOKIEEEEE <3
-app.use(cookieSession({
-    maxAge: 24*60*60*1000,
-    keys: [keys.session.cookieKey]
-}));
+// // COOKIEEEEE <3
+// app.use(cookieSession({
+//     maxAge: 24*60*60*1000,
+//     keys: [keys.session.cookieKey]
+// }));
 
-// PASSPORT INITIALIZATION 
-app.use(passport.initialize());
-app.use(passport.session());
+// // PASSPORT INITIALIZATION 
+// app.use(passport.initialize());
+// app.use(passport.session());
 
-// OAUTH ROUTES BRUH
-app.use('/api/authguard', authGuardRoute);
-app.use('/api/auth', authRoutes);
-app.use('/#/dashboard', dashboardRoutes);
+// // OAUTH ROUTES BRUH
+// app.use('/api/authguard', authGuardRoute);
+// app.use('/api/auth', authRoutes);
+// app.use('/#/dashboard', dashboardRoutes);
 
-// CONNECT TO DB BRAH
-mongoose.connect(keys.mongoDB.dbURL, () => {
-    console.log('Connected to DB');
-})
+// // CONNECT TO DB BRAH
+// mongoose.connect(keys.mongoDB.dbURL, () => {
+//     console.log('Connected to DB');
+// })
 
-// VIEW HBS
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+// // VIEW HBS
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'hbs');
 
 // HOMEPAGE STUFF 
 app.get('/', function (req, res, next) {
-    res.render('index');
+    res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 // PORT NUMBER FOR LISTENING
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("working on port 3000");
 })
